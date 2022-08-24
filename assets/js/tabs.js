8a0be5581479d93539ae2339feedb14c84317fa8
jaseem-v <muhammedjaseem753@gmail.com>
2022-08-25 02:05:31 +0530

    services function added

--- assets/js/tabs.js
+++ assets/js/tabs.js
@@ -1,11 +1,27 @@
 const tabsItem = document.querySelectorAll(".services-page__item");
+const tabs_pane = document.querySelectorAll(".services-page__card-container")
 
-tabsItem.forEach(el => {
-    el.addEventListener("click", (e) => {
+tabsItem.forEach(nav => {
+    nav.addEventListener("click", (e) => {
         tabsItem.forEach(item => {
             item.classList.remove("active")
         })
 
-        el.classList.add("active")
+        nav.classList.add("active")
+
+        tabs_pane.forEach(el => {
+            el.classList.remove("show")
+            el.classList.remove("active")
+
+            if (nav.id == el.getAttribute("tab-panel")) {
+                el.classList.add("show")
+                el.classList.add("active")
+            }
+        })
+
+
     })
 })
+
+
+
